ce7f6fd97d0897d60e55c27660168e28461d94bf
ryan <116114858+adahwbd@users.noreply.github.com>
2024-09-27 12:01:06 -0300

    Atualização_v1
    Atualização_v1

---
+++ js/principal.js
@@ -0,0 +1,32 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var tdAltura = paciente.querySelector(".info-altura");
+var tdImc = paciente.querySelector(".info-imc");
+
+var peso = tdPeso.textContent;
+var altura = tdAltura.textContent;
+
+var pesoEhvalido = true;
+var alturaEhvalida = true;
+
+if (peso <= 0 || peso >= 1000) {
+    pesoEhvalido = false;
+    tdImc.textContent = "peso invalido!"
+}
+
+if (altura <= 0 || altura >= 3.00) {
+    alturaEhvalida = false;
+    tdImc.textContent = "altura invalida!";
+}
+
+if (alturaEhvalida && pesoEhvalido) {
+    var imc = peso / (altura * altura)
+    tdImc.textContent = imc.toFixed(2);
+}
+
+
+
